c6eaa6293a83652881da97beb776b052324a2272
alepicco95 <37936360+alepicco95@users.noreply.github.com>
2020-12-22 17:49:26 +0100

    firsts entity classes

---
+++ src/logic/model/EventOrganizer.java
@@ -0,0 +1,36 @@
+package logic.model;
+
+public class EventOrganizer {
+	
+	private String firstname;
+	private String lastname;
+	private String email;
+	private String ownRooms;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getOwnRooms() {
+		return ownRooms;
+	}
+	public void setOwnRooms(String ownRooms) {
+		this.ownRooms = ownRooms;
+	}
+	
+	
+}
