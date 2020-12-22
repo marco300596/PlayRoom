46b07e266a7f2dfb793c46e20e36d28bc8185f25
flavio497 <34423187+flavio497@users.noreply.github.com>
2020-12-22 18:47:16 +0100

    aggiunto Hardware

---
+++ src/logic/model/Hardware.java
@@ -0,0 +1,36 @@
+package logic.model;
+
+public class Hardware {
+
+	private String hardwareName;
+	private int quantity;
+	private int availability;
+	private String description;
+	
+	public String getHardwareName() {
+		return hardwareName;
+	}
+	public void setHardwareName(String hardwareName) {
+		this.hardwareName = hardwareName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public int getAvaibility() {
+		return availability;
+	}
+	public void setAvaibility(int avaibility) {
+		this.availability = avaibility;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
+
