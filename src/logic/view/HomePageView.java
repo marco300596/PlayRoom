0ec66159e7cb948888cbbb1b42320dbf72215e1a
alepicco95 <37936360+alepicco95@users.noreply.github.com>
2020-12-26 18:58:26 +0100

    add simple homepage

--- src/logic/view/HomePageView.java
+++ src/logic/view/HomePageView.java
@@ -1,5 +1,27 @@
 package logic.view;
 
-public class HomePageView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class HomePageView extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+    	
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("homepage.fxml"));
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	}
+    	
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
+
