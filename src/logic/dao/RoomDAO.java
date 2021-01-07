15ce1e2fd4ae429f9d0a5fe92b6067d1b7e8fed2
flavio497 <34423187+flavio497@users.noreply.github.com>
2021-01-07 15:44:34 +0100

    added Room DAO

---
+++ src/logic/dao/RoomDAO.java
@@ -0,0 +1,43 @@
+package logic.dao;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import logic.model.Hardware;
+import logic.model.Room;
+import logic.model.VideoGame;
+
+
+public class RoomDAO {
+	public Room getRoom(String roomName) {
+		Connection connection = ConnectionFactory.getConnection();
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT * FROM room WHERE roomName=" + roomName);
+			
+			if(rs.next()) {
+				return extractRoomFromResultSet(rs);
+			}
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+	
+	private Room extractRoomFromResultSet(ResultSet rs) throws SQLException{
+		Room room = new Room();
+		
+		room.setRoomName(rs.getString("roomName"));
+		room.setNumSeat(rs.getInt("numSeat"));
+		room.setPrice(rs.getInt("price"));
+		room.setLocation(rs.getString("location"));
+		room.setPhoto(rs.getString("photo"));
+		room.addGame((VideoGame) rs.getArray("vg"));
+		room.addHardware((Hardware) rs.getArray("hw"));
+		
+		
+		return room;
+	}
+}
+
--- src/logic/model/Room.java
+++ src/logic/model/Room.java
@@ -24,6 +24,7 @@ public class Room {
 		this.hw = new ArrayList<>();
 	}
 	
+
 	public List<Hardware> getHw(){
 		return hw;
 	}
