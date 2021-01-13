59a5844434017a7d8b35211cb7c2987fc44262db
alepicco95 <37936360+alepicco95@users.noreply.github.com>
2021-01-13 20:15:30 +0100

    try to add dialogs

--- src/logic/view/CreateTournamentViewController.java
+++ src/logic/view/CreateTournamentViewController.java
@@ -1,5 +1,6 @@
 package logic.view;
 
+import java.awt.Component;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,6 +9,7 @@ import javax.swing.JOptionPane;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import logic.controller.CreateTournamentController;
@@ -26,19 +28,29 @@ public class CreateTournamentViewController {
 
     @FXML
     private Button createBtn;
+    
+    private Label label;
 
     @FXML
     void createTournament(MouseEvent event) throws MyRuntimeException, SQLException {
     	
+    	if(nameTxt.getText().isEmpty() || catTxt.getText().isEmpty() || partTxt.getText().isEmpty()) {
+    		Component c = null;
+			int option = JOptionPane.showConfirmDialog(c,"Please fill all textfield!");
+			if (option == JOptionPane.OK_OPTION) {
+				//come cazzo faccio???
+				//createTournament();
+			}
+		}
     	CreateTournamentController controller = CreateTournamentController.getInstance();
     	controller.getBean().setTournamentName(nameTxt.getText());
     	controller.getBean().setTournamentGame(catTxt.getText());
     	int num = Integer.parseInt(partTxt.getText());
     	controller.getBean().setTournamentPartecipants(num);
+    	
     	try {
     		controller.insertNewTournament(controller.getBean());
-    		JOptionPane.showMessageDialog(null,"GZ! new tournament created!", "Success", JOptionPane.INFORMATION_MESSAGE);
-    		
+    		JOptionPane.showConfirmDialog(null,"GZ! new tournament created!", "Success", JOptionPane.INFORMATION_MESSAGE);
     	} catch(Exception e){
     		Logger.getLogger(CreateTournamentController.class.getName()).log(Level.SEVERE, null, e);
     	}
