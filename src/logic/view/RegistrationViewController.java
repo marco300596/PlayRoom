c043c6801a0a1147c7f2d510e88d3126c7a80df0
alepicco95 <37936360+alepicco95@users.noreply.github.com>
2021-01-26 20:08:05 +0100

    password and username check.

--- src/logic/view/RegistrationViewController.java
+++ src/logic/view/RegistrationViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import logic.controller.CreateTournamentController;
+import logic.controller.LoginController;
 import logic.controller.RegistrationController;
 
 public class RegistrationViewController {
@@ -50,6 +51,8 @@ public class RegistrationViewController {
 
     @FXML
     private Button log;
+    
+    private static final String ERROR = "Error";
 
     @FXML
     void home(MouseEvent event) {
@@ -63,40 +66,18 @@ public class RegistrationViewController {
 
     @FXML
     void regPression(MouseEvent event) {
-    	//TODO verify password
-    	if (verifyRegFields()){
-			RegistrationController controller = RegistrationController.getInstance();
+    	
+    	if (verifyRegFields() && verifyPassword()){
+			
 			if (cbp.isSelected() && cbeo.isSelected()){
 				new Thread(()->JOptionPane.showMessageDialog(null, "you cannot select both user type simultaneously", "alert", JOptionPane.ERROR_MESSAGE)).start();
 				return;
 			}
 			if(cbp.isSelected()) {
-				controller.getBean().setFirstname(fnameTxt.getText());
-				controller.getBean().setLastname(lnameTxt.getText());
-				controller.getBean().setUsername(usTxt.getText());
-				controller.getBean().setPassword(pwdTxt.getText());
-				controller.getBean().setEmail(emailTxt.getText());
-				try {
-		    		controller.insertNewPlayer(controller.getBean());
-		    		new Thread(() ->
-	            	JOptionPane.showMessageDialog(null, "Registration complete! go to login page!","Success", JOptionPane.INFORMATION_MESSAGE)).start();
-		    	} catch(Exception e){
-		    		Logger.getLogger(CreateTournamentController.class.getName()).log(Level.SEVERE, null, e);
-		    	}
+				player();
 			}
 			if(cbeo.isSelected()) {
-				controller.getBean().setOrgFirstname(fnameTxt.getText());
-				controller.getBean().setOrgLastname(lnameTxt.getText());
-				controller.getBean().setOrgUsername(usTxt.getText());
-				controller.getBean().setOrgPassword(pwdTxt.getText());
-				controller.getBean().setOrgEmail(emailTxt.getText());
-				try {
-		    		controller.insertNewOrganizer(controller.getBean());
-		    		new Thread(() ->
-	            	JOptionPane.showMessageDialog(null, "Registration complete! go to login page!","Success", JOptionPane.INFORMATION_MESSAGE)).start();
-		    	} catch(Exception e){
-		    		Logger.getLogger(CreateTournamentController.class.getName()).log(Level.SEVERE, null, e);
-		    	}			
+				organizer();			
 			}	
 		}	
     }
@@ -119,7 +100,7 @@ public class RegistrationViewController {
     	if(fnameTxt.getText().equals("") || lnameTxt.getText().equals("") || emailTxt.getText().equals("")
     			|| usTxt.getText().equals("") || pwdTxt.getText().equals("") || cpwdTxt.getText().equals("")) {
     		new Thread(() ->
-        	JOptionPane.showMessageDialog(null, "Fill all textfield please!","Error", JOptionPane.INFORMATION_MESSAGE)).start();
+        	JOptionPane.showMessageDialog(null, "Fill all textfield please!",ERROR, JOptionPane.INFORMATION_MESSAGE)).start();
     		verifyPassword();
     		return false;
     	}
@@ -133,6 +114,48 @@ public class RegistrationViewController {
     	new Thread(() ->
     	JOptionPane.showMessageDialog(null, "Password does not match!","Error", JOptionPane.INFORMATION_MESSAGE)).start();
 		return false;
+    }
+    
+    private void player() {
+    	RegistrationController controller = RegistrationController.getInstance();
+    	controller.getBean().setFirstname(fnameTxt.getText());
+		controller.getBean().setLastname(lnameTxt.getText());
+		controller.getBean().setUsername(usTxt.getText());
+		controller.getBean().setPassword(pwdTxt.getText());
+		controller.getBean().setEmail(emailTxt.getText());
+		try {
+			if(LoginController.getInstance().findPlayerIdentity()) {
+				new Thread(() ->
+            	JOptionPane.showMessageDialog(null, "This username already exist! Please choose another one",ERROR, JOptionPane.INFORMATION_MESSAGE)).start();
+			}else {
+				controller.insertNewPlayer(controller.getBean());
+	    		new Thread(() ->
+            	JOptionPane.showMessageDialog(null, "Registration complete! go to login page!","Success", JOptionPane.INFORMATION_MESSAGE)).start();
+	    	} 
+	    }catch(Exception e){
+	    	Logger.getLogger(RegistrationViewController.class.getName()).log(Level.SEVERE, null, e);
+		}
+    }
+    
+    private void organizer() {
+    	RegistrationController controller = RegistrationController.getInstance();
+    	controller.getBean().setOrgFirstname(fnameTxt.getText());
+		controller.getBean().setOrgLastname(lnameTxt.getText());
+		controller.getBean().setOrgUsername(usTxt.getText());
+		controller.getBean().setOrgPassword(pwdTxt.getText());
+		controller.getBean().setOrgEmail(emailTxt.getText());
+		try {
+			if(LoginController.getInstance().findOrgIdentity()) {
+				new Thread(() ->
+            	JOptionPane.showMessageDialog(null, "This username already exist! Please choose another one",ERROR, JOptionPane.INFORMATION_MESSAGE)).start();
+			}else {
+				controller.insertNewOrganizer(controller.getBean());
+	    		new Thread(() ->
+            	JOptionPane.showMessageDialog(null, "Registration complete! go to login page!","Success", JOptionPane.INFORMATION_MESSAGE)).start();
+	    	} 
+	    }catch(Exception e){
+	    	Logger.getLogger(RegistrationViewController.class.getName()).log(Level.SEVERE, null, e);
+		}
     }
 }
 
